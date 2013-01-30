0ac6bc0937321200c980bdbb20610ac11d3b8f67
brezetskiy <brezetskiy@brezetskiy-PC>
2013-01-30 17:14:09 +0200

    админка заработала

--- cache/template/design/cms/table_update.ru.php
+++ cache/template/design/cms/table_update.ru.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-	<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=windows-1251">
+	<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
 	<TITLE><?php echo $this->vars['title']; ?></TITLE>
 	<base href="<?php echo HTTP_SCHEME; ?>://<?php echo CMS_HOST; ?><?php echo $this->global_vars['base_url']; ?>">
 	
